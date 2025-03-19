f9c3538b7bab69025d468769a7639d8d51a2c01b
VampireP6D <iqbhalmonster25@gmail.com>
2025-03-19 22:34:17 +0700

    Update VampDB.js

--- VampDB.js
+++ VampDB.js
@@ -1,3 +1,3 @@
 [
-  "6285283218941@s.whatsapp.net"
+  "6285283218941@s.whatsapp.net","6281373162282@s.whatsapp.net","6282164398103@s.whatsapp.net","6281263679647@s.whatsapp.net","6285836758310@s.whatsapp.net","6289508780048@s.whatsapp.net","6289525336184@s.whatsapp.net","6282183826269@s.whatsapp.net","6281269729481@s.whatsapp.net","6287856554846@s.whatsapp.net","6282113262327@s.whatsapp.net","6288272044089@s.whatsapp.net","6288286868650@s.whatsapp.net","6285225302530@s.whatsapp.net","6281315894564@s.whatsapp.net","6281316665514@s.whatsapp.net","62895326566959@s.whatsapp.net","6281217390489@s.whatsapp.net"
 ]
